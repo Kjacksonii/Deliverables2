a6c2862c7639385b5ab9ce106c969f7c50209698
Kjacksonii <82187409+Kjacksonii@users.noreply.github.com>
2021-05-10 11:28:21 -0400

    Add files via upload

---
+++ Deliverables2/Deliverables2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Deliverables2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string userResponse;
+            //string author = hello;
+            Console.WriteLine("Would you like to have a conversation.");
+            userResponse = Console.ReadLine();
+
+            while (userResponse == "yes")
+            {
+                //string lcaseAuthor = author.ToLower();
+                //Console.WriteLine($"Lowercase: {lcaseAuthor}");
+                userResponse = Console.ReadLine();
+                if (userResponse == "hello")
+                {
+
+                    Console.Write("Hi good to see you ");
+
+                    Console.Write("What's up?");
+
+                }
+                else if (userResponse == "sup")
+                {
+
+                    Console.Write("I am good ");
+
+                    Console.Write("Obi-Wan?");
+
+                }
+                else if (userResponse == "hello there")
+                {
+                    Console.Write("General Kenobi ");
+
+                    Console.Write("What's up?");
+
+                }
+                else if (userResponse == "bye")
+                {
+                    Console.Write("Good bye!");
+                    System.Environment.Exit(1);
+                }
+                else
+                {
+                    Console.Write("I don't understand");
+                }
+            }
+
+        }
+    }
+}
