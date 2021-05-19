585d4c0e6bb423763f795553db89dd5882b3ef3f
Kjacksonii <82187409+Kjacksonii@users.noreply.github.com>
2021-05-19 16:54:15 -0400

    Add files via upload

--- Deliverables2/Deliverables2/Program.cs
+++ Deliverables2/Deliverables2/Program.cs
@@ -6,51 +6,40 @@ namespace Deliverables2
     {
         static void Main(string[] args)
         {
-            
+
             string userResponse;
             //string author = hello;
-            Console.WriteLine("Would you like to have a conversation.");
+            Console.WriteLine("What do you want to say to the bot?");
+
             userResponse = Console.ReadLine();
-
-            while (userResponse == "yes")
+            if (userResponse.ToLower() == "hello")
             {
-                //string lcaseAuthor = author.ToLower();
-                //Console.WriteLine($"Lowercase: {lcaseAuthor}");
-                userResponse = Console.ReadLine();
-                if (userResponse == "hello")
-                {
 
-                    Console.Write("Hi good to see you ");
-
-                    Console.Write("What's up?");
-
-                }
-                else if (userResponse == "sup")
-                {
-
-                    Console.Write("I am good ");
-
-                    Console.Write("Obi-Wan?");
-
-                }
-                else if (userResponse == "hello there")
-                {
-                    Console.Write("General Kenobi ");
-
-                    Console.Write("What's up?");
-
-                }
-                else if (userResponse == "bye")
-                {
-                    Console.Write("Good bye!");
-                    System.Environment.Exit(1);
-                }
-                else
-                {
-                    Console.Write("I don't understand");
-                }
+                Console.WriteLine("Hi good to see you");
+                Console.WriteLine("What else do want to say?");
             }
 
+            userResponse = Console.ReadLine();
+            if (userResponse.ToLower() == "sup")
+            {
+                Console.WriteLine("I am good ");
+                Console.WriteLine("What else do want to say?");
+
+            }
+             userResponse = Console.ReadLine();
+             if (userResponse.ToLower() == "hello there")
+            { 
+                Console.WriteLine("General Kenobi");
+                Console.WriteLine("What else do want to say?");
+                userResponse = Console.ReadLine();
+
+            }
+ 
+            else _ = userResponse.ToLower() == "bye";
+            {
+                Console.WriteLine("Good bye!");
+                System.Environment.Exit(1);
+            }
         }
     }
 }
