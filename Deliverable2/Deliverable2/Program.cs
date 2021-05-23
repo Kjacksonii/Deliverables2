d809fe2419e24c40f8d88d49560a02be918d65e5
Kjacksonii <82187409+Kjacksonii@users.noreply.github.com>
2021-05-22 22:40:15 -0400

    Add files via upload

---
+++ Deliverable2/Deliverable2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Deliverable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            string comOutput = "";
+            string lastInput = "";
+            bool foundResponse;
+            bool shutdown = false;
+
+            Console.WriteLine("What do you want to say to the bot?");
+
+            while (!shutdown)
+            {
+
+                foundResponse = false;
+
+                Console.Write("User: ");
+                userInput = Console.ReadLine().ToLower();
+
+                Console.Write("Computer: ");
+                if (lastInput != userInput)
+                {
+                    switch (userInput)
+                    {
+                        case "hello":
+                            comOutput = "Hi good to see you";
+                            foundResponse = true;
+                            break;
+                        case "sup":
+                            comOutput = "I am good";
+                            foundResponse = true;
+                            break;
+
+                        case "hello there":
+                            comOutput = "General Kenobi";
+                            foundResponse = true;
+                            break;
+
+                        case "bye":
+                            Console.WriteLine("Good bye!");
+                            System.Environment.Exit(1);
+                            foundResponse = true;
+                            break;
+                    }
+                    if (foundResponse)
+                    {
+                        Console.WriteLine(comOutput);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("I’m sorry but you have already said that");
+                }
+                lastInput = userInput;
+            }
+        }
+    }
+}
